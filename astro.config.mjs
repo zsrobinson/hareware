fcb2d20db35a9dc65faab9bb709aed291414f4e4
Zachary Robinson <me@zsrobinson.com>
2024-10-04 15:49:50 -0400

    add vercel adapter, other changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind({ applyBaseStyles: false }), react()],
+  adapter: vercel(),
 });

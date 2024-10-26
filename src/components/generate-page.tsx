09cf1abdffb4cb3ccef7f1d7ddcd189d1dbe0229
Zachary Robinson <me@zsrobinson.com>
2024-10-25 22:55:09 -0400

    add warning for overflow

--- src/components/generate-page.tsx
+++ src/components/generate-page.tsx
@@ -1,12 +1,17 @@
-import { DownloadIcon } from "@radix-ui/react-icons";
+import {
+  CheckCircledIcon,
+  DownloadIcon,
+  ExclamationTriangleIcon,
+} from "@radix-ui/react-icons";
 import download from "downloadjs";
 import { toPng } from "html-to-image";
-import { useEffect, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { useLayoutState } from "~/lib/layout-state";
 import { ContentSlide } from "./content-slide";
 import { OptionsForm } from "./options-form";
 import { TitleSlide } from "./title-slide";
 import { Button } from "./ui/button";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export function GeneratePage({
   defaultTitleContent,
@@ -20,40 +25,56 @@ export function GeneratePage({
   children: ReactNode;
 }) {
   const state = useLayoutState();
+  const [isOverflowing, setIsOverflowing] = useState(false);
+  const [animate] = useAutoAnimate();
 
   useEffect(() => {
     state.setTitleContent(defaultTitleContent);
     state.setAuthorContent(defaultAuthorContent);
   }, []);
 
+  useEffect(() => {
+    const titleSlide = document.getElementById("title-slide")!;
+    setIsOverflowing(titleSlide.scrollHeight > titleSlide.clientHeight);
+  }, [state]);
+
   return (
     <div className="flex flex-col items-start gap-8 md:flex-row">
       <OptionsForm />
 
       <div className="flex flex-wrap items-start gap-8">
-        <div className="flex flex-col items-center gap-4 rounded-xl bg-zinc-100 p-4">
+        <div className="flex flex-col items-center gap-4 rounded-xl bg-secondary p-4">
           <TitleSlide imageURI={imageURI} />
-          <Button
-            variant="outline"
-            id="title-slide-download"
-            onClick={() => {
-              const titleSlide = document.getElementById("title-slide")!;
-              new Promise((res) => setTimeout(res, 500)).then(() =>
-                toPng(titleSlide, {
-                  canvasHeight: 1000,
-                  canvasWidth: 1000,
-                }).then((dataURI) => {
-                  download(dataURI, "title-slide.png");
-                }),
-              );
-            }}
-          >
-            <DownloadIcon className="mr-2" />
-            Download
-          </Button>
+          <div className="flex w-full justify-around" ref={animate}>
+            <Button
+              variant="outline"
+              id="title-slide-download"
+              onClick={() => {
+                const titleSlide = document.getElementById("title-slide")!;
+                new Promise((res) => setTimeout(res, 500)).then(() =>
+                  toPng(titleSlide, {
+                    canvasHeight: 1000,
+                    canvasWidth: 1000,
+                  }).then((dataURI) => {
+                    download(dataURI, "title-slide.png");
+                  }),
+                );
+              }}
+            >
+              <DownloadIcon className="mr-2" />
+              Download
+            </Button>
+
+            {isOverflowing && (
+              <div className="flex items-center gap-2 text-sm font-medium text-destructive">
+                <ExclamationTriangleIcon className="mt-0.5" />
+                <span>Content may be overflowing</span>
+              </div>
+            )}
+          </div>
         </div>
 
-        <div className="flex flex-col items-center gap-4 rounded-xl bg-zinc-100 p-4">
+        <div className="flex flex-col items-center gap-4 rounded-xl bg-secondary p-4">
           <ContentSlide>{children}</ContentSlide>
           <Button
             variant="outline"
